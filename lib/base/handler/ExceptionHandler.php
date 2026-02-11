484a7054f0869bf296c9f13a9b8c703a8dd665ae
TravSchan <accounts@travschan.com>
2026-02-11 10:21:26 +0700

    Exception Handler

---
+++ lib/base/handler/ExceptionHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+
+# Attoya Include
+include_once VENDOR_ROOT . '/attoya/exception/AttoyaExceptionHandler.php';
+
+
+
+
+#
+# Exception Class
+#
+class ExceptionHandler extends AttoyaExceptionHandler {
+
+
+
+
+}
+
+
+
